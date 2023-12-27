d5ae66fb7339b8de31a2a78be986631ad85a1d59
Prajwal Dhungana <dhunganaprajwal1@gmail.com>
2023-12-27 12:55:40 -0600

    Update completion status and order of to-do items

--- src/slices/todoSlice.js
+++ src/slices/todoSlice.js
@@ -6,8 +6,8 @@ export const todoSlice = createSlice({
     initialState: {
         value: [
             {id: 'cb2039b4-1b5a-4297-b146-aca0695c7899', todo: 'Hello there, welcome to my to-do list app', completion: false},
-            {id: 'cb2039b4-1b5a-4297-b146-aca06sdauh99', todo: 'You can click me or the circle to my left to mark me as completed', completion: true},
-            {id: 'cb2039b4-1b5a-4397-b146-acdagsdavh99', todo: 'You can click or tap on the trash icon to delete me', completion: false},
+            {id: 'cb2039b4-1b5a-4297-b146-aca06sdauh99', todo: 'You can click me or the circle to my left to mark me as completed', completion: false},
+            {id: 'cb2039b4-1b5a-4397-b146-acdagsdavh99', todo: 'You can click or tap on the trash icon to delete me', completion: true},
         ]
     },
     reducers: {
