1326806e0ac5cfc3012a097b19bc6e9182f50dac
Prajwal Dhungana <dhunganaprajwal1@gmail.com>
2023-12-27 12:54:24 -0600

    Update todoSlice.js with new task descriptions

--- src/slices/todoSlice.js
+++ src/slices/todoSlice.js
@@ -5,8 +5,8 @@ export const todoSlice = createSlice({
     name: 'todo',
     initialState: {
         value: [
-            {id: 'cb2039b4-1b5a-4297-b146-aca0695c7899', todo: 'Hello there, welcome to my to-do list app!', completion: false},
-            {id: 'cb2039b4-1b5a-4297-b146-aca06sdauh99', todo: 'This is what a completed task looks like', completion: true},
+            {id: 'cb2039b4-1b5a-4297-b146-aca0695c7899', todo: 'Hello there, welcome to my to-do list app', completion: false},
+            {id: 'cb2039b4-1b5a-4297-b146-aca06sdauh99', todo: 'You can click me or the circle to my left to mark me as completed', completion: true},
             {id: 'cb2039b4-1b5a-4397-b146-acdagsdavh99', todo: 'You can click or tap on the trash icon to delete me', completion: false},
         ]
     },
